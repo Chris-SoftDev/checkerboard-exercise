7dd88aa124a49d004a3e4e7457023cc3e1f5ebf6
Christopher Ronca <christopherronca@gmail.com>
2022-12-14 17:06:54 -0700

    Added Random Colors

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
 // Your JS goes here
-function squareBlack(parent){
+function squareRandomColor(parent, numGen){
     let square = document.createElement('div');
     square.style.float = 'left';
     square.style.paddingBottom = '11.1%';
     square.style.width = '11.1%';
     square.style.height = '11.1%';
-    square.style.backgroundColor = 'black';
+    square.style.backgroundColor = colorArr[numGen(0, colorArr.length-1)];
     parent.appendChild(square)
 }
 
-function squareRed(parent){
-    let square = document.createElement('div');
-    square.style.float = 'left';
-    square.style.paddingBottom = '11.1%';
-    square.style.width = '11.1%';
-    square.style.height = '11.1%';
-    square.style.backgroundColor = 'red';
-    parent.appendChild(square)
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+var colorArr = ['red', 'green', 'blue', 'purple', 'yellow', 'white', 'black', 'grey', 'aqua', 'aquamarine', 'blueviolet', 'orange', 'brown', 'seagreen', 'darkblue', 'cyan', 'bisque', 'crimson'];
 
 const body = document.querySelector('body');
 body.style.display = 'flex';
 body.style.flexWrap = 'wrap';
 
-for (i=0; i<50; i++) {
-        squareBlack(body);
-        squareRed(body);
+for (i=0; i<100; i++) {
+    squareRandomColor(body, randomNumber);
 }
