f54b646de2999c098dd756a0bfd0bbf8ac377d17
Christopher Ronca <christopherronca@gmail.com>
2022-12-14 22:16:00 -0700

    Flashing Colors Checkboard Completed - Code Refactored

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// Your JS goes here
-function squareRandomColor(numGen){
+function squareRandomColor(){
     let square = document.createElement('div');
     square.style.float = 'left';
     square.style.paddingBottom = '11.1%';
     square.style.width = '11.1%';
     square.style.height = '11.1%';
-    square.style.backgroundColor = colorArr[numGen(0, colorArr.length-1)];
+    square.style.backgroundColor = colorArr[randomNumber(0, colorArr.length-1)];
     body.appendChild(square)
 }
 
@@ -14,28 +13,17 @@ function randomNumber(min, max) {
 }
 
 function flashingSquares() {
-    // body.innerHTML = '';
+    body.innerHTML = '';
     for (i=0; i<75; i++) {
-        squareRandomColor(randomNumber);
+        squareRandomColor();
     }
 }
 
-function bgAudio() {
-    let audio = document.createElement('embed');
-    audio.src = 'music.mp3';
-    audio.autoplay = 'autoplay';
-    audio.loop = 'true';
-    // audio.hidden = 'true';
-    body.appendChild(audio);
-}
-
-var time = 1;
 var colorArr = ['red', 'green', 'blue', 'purple', 'yellow', 'white', 'black', 'grey', 'aqua', 'aquamarine', 'blueviolet', 'orange', 'brown', 'seagreen', 'darkblue', 'cyan', 'bisque', 'crimson'];
 
 const body = document.querySelector('body');
 body.style.display = 'flex';
 body.style.flexWrap = 'wrap';
 
-bgAudio();
 flashingSquares();
 setInterval(flashingSquares, 2000);
